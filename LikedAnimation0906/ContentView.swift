432428f46d8c3e7f8c08ea748e904bc6da1119b8
张亚飞 <672233580@qq.com>
2021-09-06 12:56:07 +0800

    Initial Commit

---
+++ LikedAnimation0906/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LikedAnimation0906
+//
+//  Created by 张亚飞 on 2021/9/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LikedAnimation0906/LikedAnimation0906App.swift
@@ -0,0 +1,17 @@
+//
+//  LikedAnimation0906App.swift
+//  LikedAnimation0906
+//
+//  Created by 张亚飞 on 2021/9/6.
+//
+
+import SwiftUI
+
+@main
+struct LikedAnimation0906App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
